e9eaf4c2969494e7546ef2843b69cabfae8a03d5
Ilya Kantor <iliakan@gmail.com>
2016-02-12 00:30:55 +0300

    deploy

--- public/pack/styles.535dec96d8ba7c4d2528.js
+++
@@ -1,2 +0,0 @@
-var styles = webpackJsonp_name_([ 25 ], [ function(n, a) {} ]);
-//# sourceMappingURL=styles.535dec96d8ba7c4d2528.js.map
---
+++ public/pack/styles.5654ffbfac3b486e03ca.js
@@ -0,0 +1,2 @@
+var styles = webpackJsonp_name_([ 25 ], [ function(n, a) {} ]);
+//# sourceMappingURL=styles.5654ffbfac3b486e03ca.js.map
