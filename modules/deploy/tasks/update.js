a9d9753c300cf70dcbcb9eb5950171955c81ce84
Ilya Kantor <iliakan@gmail.com>
2016-03-02 12:00:08 +0300

    minor

--- modules/deploy/tasks/update.js
+++ modules/deploy/tasks/update.js
@@ -28,13 +28,13 @@ module.exports = function() {
         yield* client.runInTarget(`git merge origin/${config.deploy.productionBranch} --no-edit`);
 
         // if there any migrations, this will stop the server and apply them
-        yield* client.runInTarget(`npm run gulp deploy:migrate`);
+        yield* client.runInTarget(`npm --silent run gulp deploy:migrate`);
 
         yield* client.runInTarget(`/usr/local/bin/pm2 startOrGracefulReload ecosystem.json --env ${args.host}`);
-        yield* client.runInTarget(`npm run gulp cache:clean`);
-        yield* client.runInTarget(`npm run gulp cloudflare:clean | bunyan`);
+        yield* client.runInTarget(`npm --silent run gulp cache:clean`);
+        yield* client.runInTarget(`npm --silent run gulp cloudflare:clean | bunyan`);
         if (args.withNginx) {
-          yield* client.runInTarget(`npm run gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs --sslEnabled`);
+          yield* client.runInTarget(`npm --silent run gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs --sslEnabled`);
           yield* client.runInTarget(`/etc/init.d/nginx reload`);
         }
       } finally {
