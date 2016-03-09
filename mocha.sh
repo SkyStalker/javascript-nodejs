6a8263d6ec6831b1731c2e3e1241d1f7f5709b3c
Ilya Kantor <iliakan@gmail.com>
2016-03-09 21:40:55 +0300

    minor

--- mocha.sh
+++ mocha.sh
@@ -8,5 +8,4 @@
 
 # tried also gulp-mocha and node `which gulp` test,
 # but it hangs after tests, not sure why, mocha.sh works fine so leave it as is
-NODE_ENV=development gulp init
-NODE_ENV=test mocha --reporter spec --colors --timeout 100000 --require lib/mongoose --require should --require co --require co-mocha --recursive --ui bdd -d $*
+ NODE_PATH=./handlers:./modules NODE_ENV=test mocha --reporter spec --colors --timeout 100000 --require lib/mongoose --require should --require co --require co-mocha --recursive --ui bdd -d $*
