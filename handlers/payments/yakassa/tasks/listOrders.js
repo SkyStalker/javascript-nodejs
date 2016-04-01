281a75149e498d68c594222e0ac3825b9b21e8af
Ilya Kantor <iliakan@gmail.com>
2016-04-01 16:37:19 +0300

    minor

--- handlers/payments/yakassa/tasks/listOrders.js
+++ handlers/payments/yakassa/tasks/listOrders.js
@@ -28,7 +28,8 @@ module.exports = function() {
         outputFormat:                       'XML',
         shopId:                             yakassaConfig.shopId,
         orderCreatedDatetimeLessOrEqual:    args.to + 'T23:59:59.999+03:00',
-        orderCreatedDatetimeGreaterOrEqual: args.from + 'T00:00:00.000+03:00'
+        orderCreatedDatetimeGreaterOrEqual: args.from + 'T00:00:00.000+03:00',
+        outputFields: 'shopId;shopName;articleId;articleName;invoiceId;orderNumber; paymentSystemOrderNumber;customerNumber; createdDatetime;paid;orderSumAmount;orderSumCurrencyPaycash;orderSumBankPaycash;paidSumAmount;paidSumCurrencyPaycash;paidSumBankPaycash;receivedSumAmount;receivedSumCurrencyPaycash;receivedSumBankPaycash;shopSumAmount;shopSumCurrencyPaycash;shopSumBankPaycash;paymentDatetime;paymentAuthorizationTime;payerCode;payerAddress;payeeCode;paymentSystemDatetime;avisoReceivedDatetime;avisoStatus;agentId;uniLabel;avisoRegistryId'
       };
 
       let result = yield* mws.sendFormRequest('listOrders', params);
