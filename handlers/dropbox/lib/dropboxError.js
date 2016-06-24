4156cf954f2ccae5eac8723546b510e0c4441509
Ilya Kantor <iliakan@gmail.com>
2016-06-24 10:02:50 +0300

    hotfix

--- handlers/dropbox/lib/dropboxError.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = class DropboxError extends Error {
-  constructor(response) {
-    
-    super(response.error_summary);
-    this.response = response;
-    this.message = response.error_summary;
-  }
-};
