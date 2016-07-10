fcd9ee230e9e7d1c2bf15965bccf5ed06108faca
Ilya Kantor <iliakan@gmail.com>
2016-07-10 09:30:01 +0300

    disable yandexmoney

--- handlers/ebook/lib/paymentMethods.js
+++ handlers/ebook/lib/paymentMethods.js
@@ -2,7 +2,7 @@ const payments = require('payments');
 
 var paymentMethods = {};
 
-var methodsEnabled = ['yakassa', 'interkassa', 'webmoney', 'yandexmoney', 'payanyway', 'paypal'];
+var methodsEnabled = ['yakassa', 'interkassa', 'webmoney'/*, 'yandexmoney',*/ 'payanyway', 'paypal'];
 
 methodsEnabled.forEach(function(key) {
   paymentMethods[key] = payments.methods[key].info;
