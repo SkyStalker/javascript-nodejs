848323868a5f167f526f8fda79edba7465d0df9f
Ilya Kantor <iliakan@gmail.com>
2016-08-01 12:22:02 +0300

    minor

--- migrations/20160801115835-teacherAgreement2.js
+++ migrations/20160801115835-teacherAgreement2.js
@@ -8,8 +8,11 @@ exports.up = function*() {
   for (let i = 0; i < courseTeachers.length; i++) {
     let teacher = courseTeachers[i];
     yield teacher.persist({
-      teacherAgreementCompany: teacher.teacher.profileName,
-      teacherAgreementName: teacher.teacher.profileName
+      teacherAgreement: {
+        company: teacher.teacher.profileName,
+        name: teacher.teacher.profileName,
+        date: new Date(2016,3,1)
+      }
     });
   }
 };
